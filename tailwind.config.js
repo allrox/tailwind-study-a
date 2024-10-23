2bb82d383f9b866c9cb94d931fe80c224110719f
allrox <alexandre@allrox.com.br>
2024-10-22 21:01:35 -0300

    Melhorias em estrutura e CSS, movimentação do arquivo index.html para a raiz

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx,png,jpg}"],
+  content: ["./*.{html,js}","./src/**/*.{html,js,jsx,ts,tsx,png,jpg}"],
   theme: {
     extend: {},
   },
@@ -8,22 +8,55 @@ module.exports = {
     function ({ addComponents }) {
       addComponents({
         '.product-card': {
-          '@apply bg-slate-100 p-3 rounded-md hover:shadow-xl': {},
+          '@apply bg-white p-3 rounded-lg hover:shadow-xl': {},
         },
-        '.primary-button':{
-          '@apply rounded w-full bg-sky-900 hover:bg-sky-700 text-white px-5 py-2 mt-5 text-xl': {},
+        '.card-image-container':{
+          '@apply relative rounded-lg mb-3': {},
+        },
+        '.card-image':{
+          '@apply rounded mb-3': {},
+        },
+        '.bag-icon':{
+          '@apply absolute rounded-full h-8 w-8 bottom-2 right-2 p-3 m-2 bg-white bg-[length:18px_18px]' : {},
+          'background-image': 'url("./img/bag.webp")',
+          'background-repeat': 'no-repeat',
+          'background-position': 'center',
+        },
+        '.badge':{
+          '@apply absolute rounded-full top-3 left-3 px-3 py-1 bg-white text-xs font-medium': {},
+        },
+        '.favorite-icon':{
+          '@apply absolute top-3 right-3 h-9 p-2 bg-white rounded-full':{},
         },
         '.star-rating': {
-          '@apply flex flex-row mb-2': {},
+          '@apply flex flex-row h-4 mb-2 items-center gap-0.5': {},
+        },
+        '.disabled': {
+          '@apply saturate-0': {},
         },
         '.star-rating img': {
-          '@apply w-4': {},
+          '@apply w-4 h-fit hover:w-5': {},
+        },
+        '.old-price':{
+          '@apply text-base line-through text-red-600 font-bold':{},
         },
         '.pricing': {
-          '@apply text-3xl font-bold mt-2': {},
+          '@apply text-3xl text-center font-bold mt-1 tracking-tighter': {},
         },
         '.pricing span':{
           '@apply text-4xl font-bold': {},
+        },
+        '.pricing-container':{
+          '@apply container flex flex-row items-center gap-2':{},
+        },
+        '.card-title':{
+          '@apply text-xl font-bold text-sky-900': {},
+        },
+        '.card-description':{
+          '@apply text-base':{},
+        },
+        '.primary-button':{
+          '@apply rounded-lg w-full bg-sky-900 hover:bg-sky-800 text-white px-5 py-4 mt-5 text-xl': {},
         },
       });
     },
